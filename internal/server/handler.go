181509d8d034435e69603932854a90eda557f2a0
Vitaliy Liubezny <liubezny@gmail.com>
2020-12-14 10:04:17 +0300

    Implement categories handler

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,6 +1,46 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
 
 func (s *server) getCategoriesHandler(w http.ResponseWriter, r *http.Request) {
+	categories, err := s.s.GetCategories(r.Context())
+	if err != nil {
+		writeInternalError(w, err)
+		return
+	}
+
+	resp := GetCategoriesResponse{
+		Categories: make([]*Category, len(categories)),
+	}
+
+	for i, c := range categories {
+		resp.Categories[i] = &Category{
+			ID:   c.ID,
+			Name: c.Name,
+		}
+	}
+
+	body, err := json.Marshal(resp)
+	if err != nil {
+		writeInternalError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func writeInternalError(w http.ResponseWriter, err error) {
+	logrus.WithError(err).Error("internal error")
+	body, _ := json.Marshal(Error{
+		Error: "internal error",
+	})
+
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(body)
 }
